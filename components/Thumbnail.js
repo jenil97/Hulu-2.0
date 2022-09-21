1db48768af1a86aed8037fe3a4647d6852ed0d8b
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-21 16:55:15 +0530

    feat: Results

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
 
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -29,6 +33,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
