4f41f43657ab45bf6e8b9f05035cecb5ba4ad446
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-21 12:20:30 +0530

    fix: index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -13,8 +15,23 @@ export default function Home() {
 
       <Header />
       <Navbar />
-
-      {/* Result */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
