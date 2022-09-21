7186ff36556b46c51bbb44f300af1a59d33dddcb
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-09-21 13:12:08 +0530

    feat: Thumbnail

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
-const Results = () => {
-  return <div>Results</div>;
+import Thumbnail from "./Thumbnail";
+
+const Results = ({ results }) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 };
 
 export default Results;
